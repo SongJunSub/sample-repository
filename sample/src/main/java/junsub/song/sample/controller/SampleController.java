ec0f133caa28a5eacb4dd624a6836ef448194533
S911130-PC\s911130-PC <s911130@sanhait.co.kr>
2022-01-24 20:01:43 +0900

    modify

--- sample/src/main/java/junsub/song/sample/controller/SampleController.java
+++ sample/src/main/java/junsub/song/sample/controller/SampleController.java
@@ -1,4 +1,16 @@
 package junsub.song.sample.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class SampleController {
+
+    @GetMapping("/helloworld")
+    public Object helloWorld(){
+
+        return "Hello World!";
+
+    }
+
 }
